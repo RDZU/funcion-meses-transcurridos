c8895c1fa66f4e29f9fa7da28e733b538cc9f746
Randolph David <davidzamorau@gmail.com>
2022-12-20 10:58:40 +0100

    version subida a produccion

--- test.php
+++ test.php
@@ -13,31 +13,18 @@ function getDiasMes($fecha) {
 
 //getDiasMes($fechaDesde->format('d-m-Y'));
 
-function get_meses_transcurridos($fechaDesde, $fechaHasta) {  
-
+function get_meses_transcurridos($fechaDesde, $fechaHasta) 
+{  
+    date_default_timezone_set('UTC');
     $fechainicial = new DateTime($fechaDesde);
     $fechafinal = new DateTime($fechaHasta);//'2013-01-01'
+    $fechainicial->modify('first day of this month');
+    $fechafinal->modify('first day of this month');
     $diferencia = $fechainicial->diff($fechafinal);
-    $dia_inicial = get_dia($fechaDesde);
-    echo "dia inicial: ". $dia_inicial; echo "\n";
-    //exit();
-    print_r("diff dias: ". $diferencia->d); echo "\n"; //exit(); 
-    print_r("suma: ". $diferencia->d + $dia_inicial); echo "\n";// exit();
-    // El método diff nos devuelve un objeto del tipo DateInterval,
-    // que almacena la información sobre la diferencia de tiempo 
-    // entre fechas (años, meses, días, etc.).
-    //exit();
-   // echo $month_2; exit();
-
-
     $meses = ( $diferencia->y * 12 ) + $diferencia->m;
-    //echo $meses + 1;
-
-    if($diferencia->d + $dia_inicial > getDiasMes($fechafinal->format('d-m-Y')))
-      return "meses transcurridos: -> ".$meses + 2;
-    else
-      return "meses transcurridos: -> ".$meses + 1;
+    return $meses + 1;
 }
+
 
 
 echo get_meses_transcurridos('04-11-2022','07-12-2022');
